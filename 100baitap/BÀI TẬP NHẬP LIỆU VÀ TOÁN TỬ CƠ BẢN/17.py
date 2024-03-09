f18688866bd91461b85218d531162f7b21485873
tranquoctrung257 <tt3617089@gmail.com>
2024-03-09 18:37:00 +0700

    update

---
+++ 100baitap/BÀI TẬP NHẬP LIỆU VÀ TOÁN TỬ CƠ BẢN/17.py
@@ -0,0 +1,11 @@
+# Nhập vào 3 số a, b, c. Hãy sắp xếp 3 số a, b, c theo thứ tự tăng dần rồi in ra lại
+
+a = int(input("nhập a từ bàn phím: "))
+b = int(input("nhập b từ bàn phím: "))
+c = int(input("nhập c từ bàn phím: "))
+
+m = a
+
+if a> b:
+    m = a
+    
